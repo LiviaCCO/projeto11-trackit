7254aa71c22c77d33ef2957ec211f6101449eb91
LiviaCCO <livia.ccapistrano@gmail.com>
2023-03-20 17:50:29 -0300

    versao para vercel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import ResetStyle from './style/ResetStyle';
 import GlobalStyle from "./style/GlobalStyle";
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
